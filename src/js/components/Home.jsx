26669c0417be1133e70432774cfd5caaa28f3706
Bryan Navarro <diazbnavarro@gmail.com>
2025-10-23 01:19:54 +0000

    task counter and adding to list done, still to make clos button dissapear

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [task, setTask] = useState('')
+	const [list, setList] = useState([])
+
+	const handletask = (event) => {
+		setTask(event.target.value)
+	}
+
+	const addtoList =(event)=>{
+		if (event.key==='Enter') {
+			if (task!=''){
+				setList([...list, task])
+				setTask('')
+			}
+		}
+	}
+	const deleteTask=(todelete)=>{
+		setList(prev=>prev.filter(task => task !==todelete))
+	}
+	const taskcounter=(list)=>{
+		if (list.length===0) {
+			return ('No Tasks')			
+		}else if (list.length===1) {
+			return('1 Task')
+		} else {
+			return(list.length + 'Tasks')
+		}
+	}
+
+
+
+	return (
+		<div className="text-center d-flex justify-content-center align-items-center vh-100" >
+			<div className="card paper" style={{ width: "18rem" }}>
+				<input type="text" className="form-control" id="tasker" onKeyDown={addtoList} onChange={handletask} value={task} placeholder="What need to be done?"></input>
+				<ul className="list-group list-group-flush">
+					{list.map((item, index)=>(
+						<li className="list-group-item d-flex justify-content-between">
+							<span key={index}>{item}</span>
+							<button type="button" className="btn-close" onClick={()=>deleteTask(item)} aria-label="Close"></button>
+						</li>
+						))}
+				</ul>
+				<p className='text-start ms-3 mt-2'>{taskcounter(list)}</p>
+			</div>
 		</div>
 	);
 };
